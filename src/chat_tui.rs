d3bb6b93f4e3e3d0cbe6fb0713b3a93527c215a0
Kirill Ivanov <bragov4ik@hotmail.com>
2021-04-16 19:18:21 +0300

    Add generics, improve docs, adjust visibility of funcs, add multi user demo

--- src/chat_tui.rs
+++ src/chat_tui.rs
@@ -135,7 +135,7 @@ impl Clone for Message {
 ///     }
 /// }
 /// ```
-pub fn build_messages_string_list(messages: Vec<Message>, max_rows: usize, columns: usize) -> Vec<String> {
+pub fn build_messages_string_list<T: fmt::Display>(messages: Vec<T>, max_rows: usize, columns: usize) -> Vec<String> {
     let mut result = vec!();
     for msg in messages {
         // Stop on exceeding terminal frame
@@ -165,7 +165,7 @@ pub fn build_messages_string_list(messages: Vec<Message>, max_rows: usize, colum
 
 /// Writes messages within specified frame. Assumes that cursor is located at the
 /// lower left corner of the frame. Leaves cursor at the same column of upper line.
-fn add_messages(messages: Vec<Message>, max_rows: usize, columns: usize) {
+fn add_messages<T: fmt::Display>(messages: Vec<T>, max_rows: usize, columns: usize) {
     let messages_list = build_messages_string_list(messages, max_rows, columns);
     let messages_list_iter = messages_list.into_iter().rev();
     let start_column = crossterm::cursor::position().unwrap().0;
@@ -211,8 +211,14 @@ fn add_empty_line(columns: u16) {
     ).unwrap(); 
 }
 
-
-pub fn draw_window(messages: Vec<Message>) {
+/// Draws window with provided messages
+/// 
+/// # Arguments
+/// 
+/// * `messages` - vector with entries of any type that implement [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html) trait ordered by newest.
+/// Newest message will be printed at the bottom of the screen.
+/// 
+pub fn draw_window<T: fmt::Display>(messages: Vec<T>) {
 
     let (columns_u16, rows_u16) = crossterm::terminal::size().unwrap();
     queue!(
@@ -259,7 +265,7 @@ pub fn draw_window(messages: Vec<Message>) {
     stdout().flush().unwrap();
 }
 
-pub fn move_cursor_to_input_field() {
+fn move_cursor_to_input_field() {
     let rows_u16 = crossterm::terminal::size().unwrap().1;
     execute!(
         stdout(), 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod chat_tui;
 
+use std::sync::{Arc, Mutex};
 use std::str::FromStr;
+use rand::seq::IteratorRandom;
 
-fn main() {
+fn one_user_chat() {
     chat_tui::open_window();    
     let mut test_vec = vec!(
         chat_tui::Message::from_strings(
@@ -46,4 +48,92 @@ fn main() {
     }
 
     chat_tui::close_window();
+}
+
+type Shared<T> = Arc<Mutex<T>>;
+
+fn add_random_msg(msgs: &mut Shared<Vec<(String, String, String)>>, message_content: &mut Shared<String>) {
+    let time_now = format!("{}", chrono::offset::Local::now().format("%H:%M:%S"));
+    let nicknames = ["admin", "boss", "bidlo", "horek"];
+    let mut rng = rand::thread_rng();
+    let nick = nicknames.iter().choose(&mut rng).unwrap();
+    let nick = String::from_str(nick).unwrap();
+    let msg_content = String::from_str(&*(message_content.lock().unwrap())).unwrap();
+    let msg = (time_now, nick, msg_content);
+    msgs.lock().unwrap().insert(0, msg);
+}
+
+fn construct_messages(messages: Vec<(String, String, String)>) -> Vec<chat_tui::Message> {
+    let mut result = vec!();
+    for msg in messages {
+        result.push(
+            chat_tui::Message::from_strings(
+                msg.0, 
+                msg.1, 
+                msg.2, 
+            )
+        );
+    }
+    result
+}
+
+fn write_random_msgs_every_sec(mut message_content: Shared<String>) {
+    let mut messages: Shared<Vec<(String, String, String)>> = 
+    Arc::new(Mutex::new(
+        vec!(
+            (
+                String::from_str("16:31:41").unwrap(), 
+                String::from_str("system").unwrap(), 
+                String::from_str("ABOBA").unwrap(),
+            ),
+            (
+                String::from_str("16:31:43").unwrap(), 
+                String::from_str("system").unwrap(), 
+                String::from_str("cam").unwrap(),
+            ),
+            (
+                String::from_str("16:31:44").unwrap(), 
+                String::from_str("system").unwrap(), 
+                String::from_str("beef").unwrap(),
+            ),
+        )
+    ));
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+        add_random_msg(&mut messages, &mut message_content);
+        chat_tui::draw_window(construct_messages(messages.lock().unwrap().clone()));
+    }
+}
+
+fn multi_user_sim() {
+    chat_tui::open_window(); 
+    chat_tui::draw_window(vec!(""));
+
+    let message_content = Arc::new(Mutex::new(String::new()));
+
+    {
+        let message_content = message_content.clone();
+        std::thread::spawn(move || {
+            write_random_msgs_every_sec(message_content)
+        });
+    }
+    
+    let mut buf = String::new();
+
+    loop {
+        // Read message
+        chat_tui::read_input_line(&mut buf).unwrap();
+        if buf.starts_with("/exit") {
+            break
+        }
+        buf = buf.trim().to_string();
+        message_content.lock().unwrap().push_str(&buf[..]);
+        buf.clear();
+    }
+
+    chat_tui::close_window();
+}
+
+fn main() {
+    multi_user_sim()
 }
